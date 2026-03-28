9b44020fdc7de0103fda64d2af5478c182d4dda6
sebscontento <98033879+sebscontento@users.noreply.github.com>
2026-03-28 01:07:19 +0100

    Initial Commit

---
+++ hentpant/hentpantApp.swift
@@ -0,0 +1,17 @@
+//
+//  hentpantApp.swift
+//  hentpant
+//
+//  Created by Sebastian  on 28/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct hentpantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
